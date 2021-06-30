b019a38fbc0be3de71bb6fd17818ae3e88cb9c4c
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-06-30 16:54:30 +0000

    css jsx

--- src/index.js
+++ src/index.js
@@ -1,29 +1,68 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
 
-function Greeting() {
+function NetFlix() {
   return (
     <>
-      <h4>Hello world. This is Augustine Akoto</h4>
-      <ul>
-        <li>Tasha</li>
-        <li>Nala</li>
-      </ul>
-      <article>This is an article</article>
+      <MovieList />
     </>
   )
 }
 
-// const Greeting = () => {
-//   return React.createElement('h1', {}, 'Hello world. This is Augustine Akoto')
-// }
+function MovieList() {
+  return (
+    <>
+      <h1>Trending Now</h1>
+      <section className='movielist'>
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+      </section>
+    </>
+  )
+}
+
+const Movie = () => {
+  return (
+    <>
+      <div className='movie'>
+        <Image />
+        <Title />
+        <Description />
+      </div>
+    </>
+  )
+}
+
+const Title = () => {
+  return <h2>Lion King</h2>
+}
+
+const Description = () => {
+  return (
+    <h5 style={{ color: '#617d98', fontSize: '0.75rem', margin: '0.25rem' }}>
+      Simba
+    </h5>
+  )
+}
+
+const Image = () => (
+  <img
+    src='https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_UX182_CR0,0,182,268_AL_.jpg'
+    alt='picture'
+  />
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Greeting />
+    <NetFlix />
   </React.StrictMode>,
   document.getElementById('root')
 )
