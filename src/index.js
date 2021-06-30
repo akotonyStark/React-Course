445190e077ed775e7f76329655ba33ca98800f7c
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-06-30 10:35:12 +0000

    commit code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+function Greeting() {
+  return (
+    <>
+      <h4>Hello world. This is Augustine Akoto</h4>
+      <ul>
+        <li>Tasha</li>
+        <li>Nala</li>
+      </ul>
+      <article>This is an article</article>
+    </>
+  )
+}
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'Hello world. This is Augustine Akoto')
+// }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Greeting />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
---
+++ src/testing.js
@@ -0,0 +1,8 @@
+import { readFileSync } from 'fs'
+import React from 'react'
+
+const testing = () => {
+  return <div></div>
+}
+
+export default testing
